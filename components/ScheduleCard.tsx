2097fd64c2c56be97b00cd50bfa882bc03baf114
Sarthak Kumar <sarthakk@onmail.com>
2024-11-08 20:07:29 +0530

    minor bug fix

--- components/ScheduleCard.tsx
+++ components/ScheduleCard.tsx
@@ -9,13 +9,18 @@ export default function ScheduleCard({ subjectName, faculty, time = [] }: { subj
         const timee4 = timee3[0].split(':');
         return Number(timee1[0]) - Number(timee4[0]) + ((Number(timee1[0]) === Number(timee4[0])) ? Number(timee1[1]) - Number(timee4[1]) : 0);
     });
+
+    const returnTime = () => {
+        const timeStr = time.join('\n');
+        return timeStr
+    }
     return (
         <View className='bg-gray-200 rounded-md p-4 flex gap-1 my-2'>
             <Text className='font-montserratSemiBold text-xl'>{subjectName}</Text>
             <Text className='font-montserrat text-xl'>{faculty}</Text>
             <Text className='font-montserrat text-xl'>
                 {
-                    time?.length === 1 ? time[0] : (time[0] + '\n' + time[1])
+                    time?.length === 1 ? time[0] : (returnTime())
                 }
             </Text>
         </View>
