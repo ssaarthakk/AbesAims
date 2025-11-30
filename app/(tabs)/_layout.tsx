44040efb32bef16182f7f187fb8ebd61dca7e610
Sarthak Kumar <realsarthakkumar@gmail.com>
2025-11-30 19:28:53 +0530

    Remove quiz tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,40 +2,42 @@ import React from 'react';
 import { Tabs } from 'expo-router';
 import { color_three, color_four } from '@/constants/Colors';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import DashboardTab from '.';
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: color_three,
-          borderTopWidth: 0,
-          elevation: 0,
-          paddingVertical: 10,
-        },
-        tabBarActiveTintColor: color_four,
-        tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.7)',
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Dashboard",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="home" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="quiz"
-        options={{
-          title: "Quiz",
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="help-circle-outline" size={size} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <DashboardTab />
+    // <Tabs
+    //   screenOptions={{
+    //     headerShown: false,
+    //     tabBarStyle: {
+    //       backgroundColor: color_three,
+    //       borderTopWidth: 0,
+    //       elevation: 0,
+    //       paddingVertical: 10,
+    //     },
+    //     tabBarActiveTintColor: color_four,
+    //     tabBarInactiveTintColor: 'rgba(255, 255, 255, 0.7)',
+    //   }}
+    // >
+    //   <Tabs.Screen
+    //     name="index"
+    //     options={{
+    //       title: "Dashboard",
+    //       tabBarIcon: ({ color, size }) => (
+    //         <Ionicons name="home" size={size} color={color} />
+    //       ),
+    //     }}
+    //   />
+    //   <Tabs.Screen
+    //     name="quiz"
+    //     options={{
+    //       title: "Quiz",
+    //       tabBarIcon: ({ color, size }) => (
+    //         <Ionicons name="help-circle-outline" size={size} color={color} />
+    //       ),
+    //     }}
+    //   />
+    // </Tabs>
   );
 }
--- app/(tabs)/quiz.tsx
+++ app/(tabs)/quiz.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { LinearGradient } from 'expo-linear-gradient';
-import { color_three, color_four } from '@/constants/Colors';
-import QuizScreen from '@/components/Quiz/QuizScreen';
+// import React from 'react';
+// import { LinearGradient } from 'expo-linear-gradient';
+// import { color_three, color_four } from '@/constants/Colors';
+// import QuizScreen from '@/components/Quiz/QuizScreen';
 
-export default function QuizTab() {
-  return (
-    <LinearGradient 
-      className='flex-1' 
-      colors={[color_three, color_four, color_three]}
-    >
-      <QuizScreen />
-    </LinearGradient>
-  );
-}
+// export default function QuizTab() {
+//   return (
+//     <LinearGradient 
+//       className='flex-1' 
+//       colors={[color_three, color_four, color_three]}
+//     >
+//       <QuizScreen />
+//     </LinearGradient>
+//   );
+// }
